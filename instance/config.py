9dd2e6de872a85a5d7a52380aea15d1aab6af7eb
didacdedm <didacdedm@hotmail.com>
2017-06-06 17:37:27 -0500

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:clave@localhost/NT_Proyecto'
+SQLALCHEMY_DATABASE_URI = 'mysql://usuario:clave@localhost/NT_Proyecto'
