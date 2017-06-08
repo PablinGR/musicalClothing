46b3e532bdf3f315ce806ea6ea251e8f3ce5716c
Unknown <didacdedm@hotmail.com>
2017-06-08 17:02:39 -0500

    Vestuarios
    Toques finales a vestuarios

--- web/app/admin/forms.py
+++ web/app/admin/forms.py
@@ -1,8 +1,11 @@
 # app/admin/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+
+from ..models import Usuario, Genero
 
 class Generoform(FlaskForm):
     """
@@ -11,3 +14,13 @@ class Generoform(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+
+class Vestuarioform(FlaskForm):
+
+	sex = SelectField(u'Sexo', choices=[('H', 'Hombre'), ('M', 'Mujer')], validators=[DataRequired()])
+	foto = StringField('Foto', validators=[DataRequired()])
+	genero = QuerySelectField(query_factor=lambda: Genero.query.all(), get_label="name", validators=[DataRequired()])
+	usuario = QuerySelectField(query_factor=lambda: Usuario.query.all(), get_label="username", validators=[DataRequired()])
+	submit = SubmitField('Submit')
--- web/app/admin/views.py
+++ web/app/admin/views.py
@@ -4,9 +4,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import Generoform
+from forms import Generoform, Vestuarioform
 from .. import db
-from ..models import Genero
+from ..models import Genero, Usuario, Vestuario
 
 def check_admin():
     """
@@ -105,3 +105,157 @@ def delete_genero(id):
     return redirect(url_for('admin.list_generos'))
 
     return render_template(title="Delete Genero")
+
+
+@admin.route('/generos/search', methods=['GET', 'POST'])
+@login_required
+def search_generos(id):
+
+    check_admin()
+
+    genero = Genero.query.get_or_404(id)
+    form.description.data = genero.description
+    form.name.data = genero.name
+    return render_template('admin/generos/genero.html', add_vestuario=add_vestuario,
+                           form=form, title="Buscar Vestuario")
+
+
+# Vestuarios Views
+
+@admin.route('/vestuarios', methods=['GET', 'POST'])
+@login_required
+def list_vestuarios():
+    check_admin()
+    vestuarios = Vestuario.query.all()
+    return render_template('admin/vestuarios/vestuarios.html', vestuarios=vestuarios, title='Vestuario')
+
+
+@admin.route('/vestuarios/add', methods=['GET', 'POST'])
+@login_required
+def add_vestuario():
+    """
+    Add a vestuario to the database
+    """
+    check_admin()
+
+    add_vestuario = True
+
+    form = VestuarioForm()
+    if form.validate_on_submit():
+        vestuario = Vestuario(sexo=form.sex.data,
+                    foto=form.foto.data, genero_id=form.genero.data,
+                    usuario_id=forms.usuario_id.data)
+
+        try:
+            # add vestuario to the database
+            db.session.add(vestuario)
+            db.session.commit()
+            flash('You have successfully added a new vestuario.')
+        except:
+            # in case vestuario name already exists
+            flash('Error: vestuario name already exists.')
+
+        # redirect to the vestuarios page
+        return redirect(url_for('admin.list_vestuarios'))
+
+    # load vestuario template
+    return render_template('admin/vestuarios/vestuarios.html', add_vestuario=add_vestuario,
+                           form=form, title='Add Vestuario')
+
+
+@admin.route('/vestuarios/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_vestuario(id):
+    """
+    Edit a vestuario
+    """
+    check_admin()
+
+    add_vestuario = False
+
+    vestuario = Vestuario.query.get_or_404(id)
+    form = VestuarioForm(obj=vestuario)
+    if form.validate_on_submit():
+        vestuario.sexo=form.sex.data
+        vestuario.foto=form.foto.data
+        vestuario.genero_id=form.genero.data
+        vestuario.usuario_id=form.usuario.data
+        db.session.add(vestuario)
+        db.session.commit()
+        flash('You have successfully edited the vestuario.')
+
+        # redirect to the vestuarios page
+        return redirect(url_for('admin.list_vestuarios'))
+
+    form.sex.data=vestuario.sexo
+    form.foto.data=vestuario.fotos
+    form.genero.data=vestuario.genero_id
+    form.usuario.data=vestuario.usuario_id
+    return render_template('admin/vestuarios/vestuario.html', search_genero=search_genero,
+                           form=form, title="Edit Vestuario")
+
+@admin.route('/vestuarios/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_vestuario(id):
+    """
+    Delete a vestuario from the database
+    """
+    check_admin()
+
+    vestuario = Vestuario.query.get_or_404(id)
+    db.session.delete(vestuario)
+    db.session.commit()
+    flash('You have successfully deleted the vestuario.')
+
+    # redirect to the vestuarios page
+    return redirect(url_for('admin.list_vestuarios'))
+
+    return render_template(title="Delete Vestuario")
+
+
+@admin.route('/vestuarios/search', methods=['GET', 'POST'])
+@login_required
+def search_vestuario(id):
+
+    check_admin()
+
+    vestuario = Vestuario.query.get_or_404(id)
+    form.sex.data=vestuario.sexo
+    form.foto.data=vestuario.foto
+    form.genero.data=vestuario.genero_id
+    form.usuario.data=vestuario.usuario_id
+    return render_template('admin/vestuarios/vestuario.html', search_vestuario=search_vestuario_vestuario,
+                           form=form, title="Buscar Vestuario")
+
+
+
+# usuario edit view 
+@admin.route('/admin/usuario/editUser', methods=['GET', 'POST'])
+@login_required  
+def editUser(): 
+    """ 
+    Handle requests to the /edit route 
+    Edit an usuario to the database through the registration form 
+    """ 
+    form = RegistrationForm() 
+    if form.validate_on_submit(): 
+        usuario = Usuario(email=form.email.data, 
+                            username=form.username.data, 
+                            first_name=form.first_name.data, 
+                            last_name=form.last_name.data, 
+                            password=form.password.data) 
+ 
+        # edit usuario to the database 
+        db.session.merge(usuario)  
+        db.session.commit() 
+        flash('You have successfully registered! You may now login.') 
+ 
+        # redirect to the login page 
+        return redirect(url_for('auth.login')) 
+ 
+    # load registration template 
+    return render_template('auth/register.html', form=form, title='Register') 
+ 
+
+
+
